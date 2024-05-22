a1b383b2d551ad3ae85cc1d5f3bbf287fba41e6c
Ryan Wilson <13529262+NxtGenCode@users.noreply.github.com>
2024-05-22 00:30:09 -0400

    Shark Area Added
    Added shark area
    Shark will move forward based on your furthest correct key in the word that was typed.

--- js/main.js
+++ js/main.js
@@ -8,7 +8,7 @@ const hideMobileDiv = document.querySelector(".hide-mobile");
 let mobileNavOpen = false;
 
 hideMobileBtn.addEventListener('click', () => {
-    if(!mobileNavOpen){
+    if (!mobileNavOpen) {
         hideMobileDiv.style.display = 'flex';
         mobileNavOpen = true;
         console.log('I ran to display the flex')
@@ -43,9 +43,19 @@ let selectedPrompt = TEXT_PROMPTS[randomPromptIndex()];
 
 let currentWordObj = selectedPrompt.split(" ");
 
-function preloadElements(){
+let completedWordCount = 0;
+let lastCorrectKeyProgress = null;
+
+const SHARK_INCREMENT_POS = 2;
+
+const player = document.getElementById('player');
+const playerShark = document.getElementById('player_shark');
+
+function preloadElements() {
     completedWords = document.createElement('span');
     completedWords.setAttribute('class', 'completed-words');
+
+    //currentProgress = (currentWordObj.length / completedWords.innerText.length);
 
     correctKeys = document.createElement('span');
     correctKeys.setAttribute('class', 'correct-keys');
@@ -67,7 +77,7 @@ function preloadElements(){
     textPrompt.append(textPromptNode);
     textPrompt.innerHTML = updatedTextPrompt();
 
-    console.log('keys left of word: '+keysLeftOfWord())
+    console.log('keys left of word: ' + keysLeftOfWord())
     gameText.prepend(textPrompt);
     gameText.prepend(keysLeft);
     gameText.prepend(nextKey);
@@ -76,229 +86,246 @@ function preloadElements(){
     gameText.prepend(completedWords);
 }
 
-function currentWordToType(){
-    if(currentIncorrectWordIndex >= 1){
+function currentWordToType() {
+    if (currentIncorrectWordIndex >= 1) {
         return currentWordObj[currentIncorrectWordIndex];
     } else {
         return currentWordObj[currentWordIndex];
     }
 }
 
-function previousWordToType(){
-    if(currentIncorrectWordIndex >= 1){
+function previousWordToType() {
+    if (currentIncorrectWordIndex >= 1) {
         return currentWordObj[currentIncorrectWordIndex - 1];
     } else {
         return currentWordObj[currentWordIndex - 1];
     }
 }
 
-function nextWordToType(){
-    if(currentIncorrectWordIndex >= 1){
+function nextWordToType() {
+    if (currentIncorrectWordIndex >= 1) {
         return currentWordObj[currentIncorrectWordIndex + 1];
     } else {
         return currentWordObj[currentWordIndex + 1];
     }
 }
 
-function nextCharToType(){
-        return currentWordToType().at(currentCharIndex);
+function nextCharToType() {
+    return currentWordToType().at(currentCharIndex);
 }
 
-function keysLeftOfWord(){
-        return currentWordToType().slice(currentCharIndex + 1);
+function keysLeftOfWord() {
+    return currentWordToType().slice(currentCharIndex + 1);
 }
 
-function updatedTextPrompt(){
+function updatedTextPrompt() {
     return textPrompt.innerText.slice(currentWordToType().length);
 }
 
-function handleInput(e){
-   const key = e.key;
+let sharkCounter = 0;
+function handleInput(e) {
+    const key = e.key;
 
-   if(key == "CapsLock" || key == "Shift"){
-    return;
-   }
-
-   //PLAYER HITS "SPACE OR ENTER TO SUBMIT"
-   if(key == " " || key == "Enter"){ 
-    //USER INPUT MATCHED WORD TO TYPE
-    if(userInputBox.value == currentWordToType()){
-        e.preventDefault();
-        correctKeys.innerHTML = "";
-        completedWords.innerHTML += currentWordToType()+" ";
-        currentCharIndex = 0;
-        currentWordIndex++;
-        currentIncorrectWordIndex++;
-        if(!currentWordToType()){
-            alert("You have finished the race! Go back to the main menu to play again!");
-            return;
-        }
-        textPrompt.innerHTML = textPrompt.innerHTML.slice(currentWordToType().length + 1);
-        nextKey.innerHTML = nextCharToType();
-        keysLeft.innerHTML = keysLeftOfWord();
-        console.log('next word..?: '+currentWordToType())
-        console.log('next char index..?: '+currentCharIndex)
-        clearInputBox();
+    if (key == "CapsLock" || key == "Shift") {
         return;
     }
-   }
+
+    //PLAYER HITS "SPACE OR ENTER TO SUBMIT"
+    if (key == " " || key == "Enter") {
+        //USER INPUT MATCHED WORD TO TYPE
+        if (userInputBox.value == currentWordToType()) {
+            e.preventDefault();
+            correctKeys.innerHTML = "";
+            completedWords.innerHTML += currentWordToType() + " ";
+            completedWordCount += 1;
+            currentCharIndex = 0;
+            currentWordIndex++;
+            currentIncorrectWordIndex++;
+            lastCorrectKeyProgress = null;
+            if (!currentWordToType()) {
+                alert("You have finished the race! Go back to the main menu to play again!");
+                return;
+            }
+            textPrompt.innerHTML = textPrompt.innerHTML.slice(currentWordToType().length + 1);
+            nextKey.innerHTML = nextCharToType();
+            keysLeft.innerHTML = keysLeftOfWord();
+            console.log('next word..?: ' + currentWordToType())
+            console.log('next char index..?: ' + currentCharIndex)
+            clearInputBox();
+            return;
+        }
+    }
 
     //Prevent user from typing so many keys
-   if(key != "Backspace" && userInputBox.value.length == 16){
-      e.preventDefault();
-      return;
-   }
-      //DELETEING KEYS START
-      if(key == "Backspace"){
+    if (key != "Backspace" && userInputBox.value.length == 16) {
+        e.preventDefault();
+        return;
+    }
+    //DELETEING KEYS START
+    if (key == "Backspace") {
         //////INCORRECT KEYS FOUND
 
         //FIX BUG FOR - If you type any other key (besides Enter or Space) after a completed word it will return null.
         //TO-DO
 
-        if(incorrectKeys.innerHTML.length >= 1){
-            console.log('currentword to type: '+currentWordToType())
-            console.log('previous to type: '+previousWordToType())
+        if (incorrectKeys.innerHTML.length >= 1) {
+            console.log('currentword to type: ' + currentWordToType())
+            console.log('previous to type: ' + previousWordToType())
             //if(currentIncorrectWordIndex >= 1 && currentCharIndex == 0 && ){
 
-        if(incorrectKeys.innerHTML.length == 1){
-            if(correctKeys.innerHTML == previousWordToType()){
-                incorrectKeys.innerHTML = "";
-                console.log('1 MILLION')
-                currentIncorrectWordIndex--;
-                currentCharIndex = currentWordToType().length;
-                textPrompt.innerHTML = " "+nextWordToType().concat('', textPrompt.innerHTML);
-                nextKey.innerHTML = "";
-                keysLeft.innerHTML = keysLeftOfWord();
-                return;
-            } else {
-            incorrectKeys.innerHTML = incorrectKeys.innerHTML.slice(1)
-            console.log('ONLY 1 KEY WAS INCORRECT')
+            if (incorrectKeys.innerHTML.length == 1) {
+                if (correctKeys.innerHTML == previousWordToType()) {
+                    incorrectKeys.innerHTML = "";
+                    console.log('1 MILLION')
+                    currentIncorrectWordIndex--;
+                    currentCharIndex = currentWordToType().length;
+                    textPrompt.innerHTML = " " + nextWordToType().concat('', textPrompt.innerHTML);
+                    nextKey.innerHTML = "";
+                    keysLeft.innerHTML = keysLeftOfWord();
+                    return;
+                } else {
+                    incorrectKeys.innerHTML = incorrectKeys.innerHTML.slice(1)
+                    console.log('ONLY 1 KEY WAS INCORRECT')
+                }
             }
-        }
-        if(incorrectKeys.innerHTML.length > 1){
-            console.log('BITCON 69 - incorrecykeys length:'+incorrectKeys.innerHTML.length);
-            incorrectKeys.innerHTML = incorrectKeys.innerHTML.slice(0, incorrectKeys.innerHTML.length - 1)
-            console.log('MORE THEN 1 KEY WAS INCORRECT')
+            if (incorrectKeys.innerHTML.length > 1) {
+                console.log('BITCON 69 - incorrecykeys length:' + incorrectKeys.innerHTML.length);
+                incorrectKeys.innerHTML = incorrectKeys.innerHTML.slice(0, incorrectKeys.innerHTML.length - 1)
+                console.log('MORE THEN 1 KEY WAS INCORRECT')
+                console.log(currentIncorrectWordIndex);
+            }
+            if (currentIncorrectWordIndex >= 1 && currentCharIndex == 0 && correctKeys.innerHTML != previousWordToType()) {
+                console.log('there was a completed word met on index 0');
+                currentIncorrectWordIndex--;
+                console.log(currentIncorrectWordIndex)
+                currentCharIndex = currentWordToType().length;
+                incorrectKeys.innerHTML = incorrectKeys.innerHTML.slice(0, incorrectKeys.innerHTML.length - 1)
+                textPrompt.innerHTML = " " + nextWordToType().concat('', textPrompt.innerHTML);
+            }
+            if (currentCharIndex <= 0) {
+                currentCharIndex = 0;
+            }
+            currentCharIndex--;
+            nextKey.innerHTML = nextCharToType();
+            keysLeft.innerHTML = keysLeftOfWord();
+
+            console.log('END OF INCORRECT KEY BACKSPACE');
             console.log(currentIncorrectWordIndex);
-        }
-        if(currentIncorrectWordIndex >= 1 && currentCharIndex == 0 && correctKeys.innerHTML != previousWordToType()){
-            console.log('there was a completed word met on index 0');
-            currentIncorrectWordIndex--;
-            console.log(currentIncorrectWordIndex)
-            currentCharIndex = currentWordToType().length;
-            incorrectKeys.innerHTML = incorrectKeys.innerHTML.slice(0, incorrectKeys.innerHTML.length - 1)
-            textPrompt.innerHTML = " "+nextWordToType().concat('', textPrompt.innerHTML);
-        }
-        if(currentCharIndex <= 0){
-            currentCharIndex = 0;
-        }
-        currentCharIndex--;
-        nextKey.innerHTML = nextCharToType();
-        keysLeft.innerHTML = keysLeftOfWord();
-        
-        console.log('END OF INCORRECT KEY BACKSPACE');
-        console.log(currentIncorrectWordIndex);
-        console.log('NEXT WORD TO WORK ON: '+currentWordToType());
-        console.log('current incorrect word index: '+currentIncorrectWordIndex);
-        return;
+            console.log('NEXT WORD TO WORK ON: ' + currentWordToType());
+            console.log('current incorrect word index: ' + currentIncorrectWordIndex);
+            return;
         }
         //////NO MORE INCORRECT KEYS TO DELETE
 
         //////CORRECT KEYS FOUND        //BACKSPACE ON CORRECT 
-        if(correctKeys.innerHTML.length >= 1){
+        if (correctKeys.innerHTML.length >= 1) {
+            if (lastCorrectKeyProgress == null) {
+                lastCorrectKeyProgress = currentCharIndex;
+            }
+                                        // pizza
+            if (lastCorrectKeyProgress < currentCharIndex) {
+                lastCorrectKeyProgress = currentCharIndex;
+                console.log("new high");
+            }
 
-        if(correctKeys.innerHTML.length == 1){
-            correctKeys.innerHTML = correctKeys.innerHTML.slice(1)
-            console.log('chase')
-        }
-        if(correctKeys.innerHTML.length > 1){
-            correctKeys.innerHTML = correctKeys.innerHTML.slice(0, correctKeys.innerHTML.length - 1)
-            console.log('michael')
-        }
-        console.log('backspace on correctkey')
-        console.log(currentWordIndex);
-        currentCharIndex--;
-        if(currentCharIndex <= 0){
-            currentCharIndex = 0;
-        }
-        keysLeft.innerHTML = keysLeftOfWord();
-        console.log('currentcharindex: '+currentCharIndex);
-        console.log(nextCharToType());
-        console.log(keysLeftOfWord());
-        nextKey.innerHTML = nextCharToType();
+            console.log("current char index: "+currentCharIndex);
+            if (correctKeys.innerHTML.length == 1) {
+                correctKeys.innerHTML = correctKeys.innerHTML.slice(1)
+                console.log('chase')
+            }
+            if (correctKeys.innerHTML.length > 1) {
+                correctKeys.innerHTML = correctKeys.innerHTML.slice(0, correctKeys.innerHTML.length - 1)
+                console.log('michael')
+            }
+            console.log('backspace on correctkey')
+            console.log(currentWordIndex);
+            currentCharIndex--;
+            if (currentCharIndex <= 0) {
+                currentCharIndex = 0;
+            }
+            keysLeft.innerHTML = keysLeftOfWord();
+            console.log('currentcharindex: ' + currentCharIndex);
+            console.log(nextCharToType());
+            console.log(keysLeftOfWord());
+            nextKey.innerHTML = nextCharToType();
             console.log('INCORRECT KEY TYPED BITCOIN OVER 9000')
-            console.log("Char Index: "+currentCharIndex+ "| Word Index: "+currentWordIndex);
+            console.log("Char Index: " + currentCharIndex + "| Word Index: " + currentWordIndex);
         }
         //////NO MORE INCORRECT KEYS TO DELETE
         //DELETEING KEYS END
+    }
+
+    //> INCORRECT KEY INPUT <
+    if (key != "Backspace" && key != nextCharToType() || key != "Backspace" && incorrectKeys.innerHTML.length >= 1) {
+        if (key == "Enter") {
+            userInputBox.value += " ";
         }
 
-   //> INCORRECT KEY INPUT <
-   if(key != "Backspace" && key != nextCharToType() || key != "Backspace" && incorrectKeys.innerHTML.length >= 1){
-    if(key == "Enter"){
-        userInputBox.value += " ";
-    }
-    
-    if(nextCharToType() != undefined){
-        incorrectKeys.innerHTML += nextCharToType();
-        currentCharIndex++;   
-    }
+        if (nextCharToType() != undefined) {
+            incorrectKeys.innerHTML += nextCharToType();
+            currentCharIndex++;
+        }
 
-    console.log('bitcoin 3031')
-    console.log(currentWordIndex)
-    console.log(currentIncorrectWordIndex)
+        console.log('bitcoin 3031')
+        console.log(currentWordIndex)
+        console.log(currentIncorrectWordIndex)
 
-    if(!nextCharToType()){
-        currentCharIndex = 0;
-        currentIncorrectWordIndex++;
-        textPrompt.innerHTML = textPrompt.innerHTML.slice(currentWordToType().length + 1);
-        incorrectKeys.innerHTML += " ";
-        nextKey.innerHTML = nextCharToType();
+        if (!nextCharToType()) {
+            currentCharIndex = 0;
+            currentIncorrectWordIndex++;
+            textPrompt.innerHTML = textPrompt.innerHTML.slice(currentWordToType().length + 1);
+            incorrectKeys.innerHTML += " ";
+            nextKey.innerHTML = nextCharToType();
+            keysLeft.innerHTML = keysLeftOfWord();
+            console.log('NULL CHAR FOUND REST CHARINDEX TO 0')
+            console.log('CurrentCharIndex:' + currentCharIndex);
+            console.log('Current IncorrectWord Index:' + currentIncorrectWordIndex);
+            console.log('Current CorrectWord Index:' + currentWordIndex);
+            return;
+        }
+
         keysLeft.innerHTML = keysLeftOfWord();
-        console.log('NULL CHAR FOUND REST CHARINDEX TO 0')
-        console.log('CurrentCharIndex:'+currentCharIndex);
-        console.log('Current IncorrectWord Index:'+currentIncorrectWordIndex);
-        console.log('Current CorrectWord Index:'+currentWordIndex);
-        return;
-    }
-    
-    keysLeft.innerHTML = keysLeftOfWord();
-    nextKey.innerHTML = nextCharToType();
-    console.log('INCORRECT KEY TYPED')
-    console.log(nextCharToType())
-    console.log("Char Index: "+currentCharIndex+ "| Word Index: "+currentWordIndex);
+        nextKey.innerHTML = nextCharToType();
+        console.log('INCORRECT KEY TYPED')
+        console.log(nextCharToType())
+        console.log("Char Index: " + currentCharIndex + "| Word Index: " + currentWordIndex);
 
     }
 
-   //> CORRECT KEY INPUT <
-   if(key == nextCharToType() && incorrectKeys.innerHTML.length == 0){
-    correctKeys.innerHTML += nextCharToType();
-    currentCharIndex++;
-    keysLeft.innerHTML = keysLeftOfWord();
+    //> CORRECT KEY INPUT <
+    if (key == nextCharToType() && incorrectKeys.innerHTML.length == 0) {
+        correctKeys.innerHTML += nextCharToType();
+        currentCharIndex++;
+        keysLeft.innerHTML = keysLeftOfWord();
+        
+        if (lastCorrectKeyProgress < currentCharIndex) {
+            sharkCounter += SHARK_INCREMENT_POS;
+            playerShark.style.left = `${sharkCounter}` + "px";
+        }
+        
+        if (!nextCharToType()) {
+            nextKey.innerHTML = "";
+            return;
+        }
 
-    if(!nextCharToType()){
-        nextKey.innerHTML = "";
-        return;
+        nextKey.innerHTML = nextCharToType();
+        console.log('CORRECT KEY TYPED')
+        console.log(nextCharToType())
+        console.log("Char Index: " + currentCharIndex + "| Word Index: " + currentWordIndex);
+
     }
 
-    nextKey.innerHTML = nextCharToType();
-    console.log('CORRECT KEY TYPED')
-    console.log(nextCharToType())
-    console.log("Char Index: "+currentCharIndex+ "| Word Index: "+currentWordIndex);
-
-   }
-
-   //TYPING KEYS END
+    //TYPING KEYS END
 }
 
-function start(){
+function start() {
     currentCharIndex = 0;
     currentWordIndex = 0;
     currentIncorrectWordIndex = 0;
     preloadElements();
     userInputBox.addEventListener('keydown', handleInput);
     console.log('Game Started.')
-    console.log('nextWord: '+currentWordToType());
+    console.log('nextWord: ' + currentWordToType());
 }
 
 start();
